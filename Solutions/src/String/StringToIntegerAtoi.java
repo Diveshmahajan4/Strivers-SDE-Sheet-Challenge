3074436be467506bd93d530c428bf470d08d5ed4
DiveshMahajan <diveshmahajan04@gmail.com>
2023-07-30 23:16:52 +0530

    Day-55

---
+++ Solutions/src/String/StringToIntegerAtoi.java
@@ -0,0 +1,45 @@
+package String;
+// https://leetcode.com/problems/string-to-integer-atoi/
+public class StringToIntegerAtoi {
+    public static void main(String[] args) {
+
+    }
+
+    public int myAtoi(String s) {
+        long ans = 0;
+        int n = s.length();
+        int i = 0;
+        int sign = 1;
+        while(i<n && s.charAt(i)==' ')
+            i++;
+
+        if(i<n && s.charAt(i)=='-')
+        {
+            sign = -1;
+            i++;
+        }
+        else if(i<n && s.charAt(i)=='+')
+            i++;
+
+        while(i<n)
+        {
+            char ch = s.charAt(i);
+            if(ch>='0' && ch<='9')
+            {
+                ans = ans*10+(ch-'0');
+                if(ans>=Integer.MAX_VALUE)
+                    break;
+            }
+            else
+                break;
+            i++;
+        }
+        ans = sign*ans;
+        if(ans>=Integer.MAX_VALUE)
+            return Integer.MAX_VALUE;
+        else if(ans<=Integer.MIN_VALUE)
+            return Integer.MIN_VALUE;
+
+        return (int)ans;
+    }
+}
